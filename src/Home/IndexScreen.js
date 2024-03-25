cbaa9d29baad5cdabe48387a8d75af829a523d41
supermanko1102 <caowenjieko@gmail.com>
2024-03-25 11:39:31 +0800

    拜託大大

--- src/Home/IndexScreen.js
+++ src/Home/IndexScreen.js
@@ -1,24 +1,48 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
+import { Text } from "react-native-paper";
 import Banner from "../Component/Banner";
 export default function IndexScreen({ navigation }) {
-  // const dispatch= useDispatch()
-  //登出功能
-  // const handleLogout =()=>{
-  //   dispatch(setLogout())
-  //   navigation.navigate("Login");
-
-  // }
+  const Data=[
+    {id:1,StartDate:'2021-01-01',subject:'第一行'},
+    {id:2,StartDate:'2021-01-01',subject:'第二行'},
+    {id:3,StartDate:'2021-01-01',subject:'第三行'},
+    {id:4,StartDate:'2021-01-01',subject:'第三行'},
+    {id:5,StartDate:'2021-01-01',subject:'第三行'},
+    {id:6,StartDate:'2021-01-01',subject:'第三行'},
+  ]
+  const renderItem=({item})=>(
+    <View
+    className='flex-row mt-6 mx-10 bg-red-400'
+    >
+      <Text
+      className='text-2xl'
+      >
+        {item.StartDate}
+      </Text>
+      <Text
+      className='mx-4 text-2xl'
+      >
+        {item.subject}
+      </Text>
+    </View>
+  )
   return (
     
     <View className="flex-1">
       <Banner/>
-      
-    {/* <TouchableOpacity
-    className="bg-red-600 rounded-lg w-10/12 m-3"
-    onPress={handleLogout}
-    >
-    <Text className="text-white text-xl m-3 text-center">登出</Text>
-      </TouchableOpacity> */}
+      <Text
+        className="text-4xl self-center mt-4">
+            最新消息
+        </Text>
+      <View 
+      className='bg-blue-300'
+      >
+        <FlatList
+          data={Data} // data
+          renderItem={renderItem} // 選染列表的函數
+          keyExtractor={item => item.id} // key
+          />
+      </View>
     </View>
 
   );
--- src/carriage/PathTracking.js
+++ src/carriage/PathTracking.js
@@ -1,26 +1,51 @@
-import { View } from "react-native"
+import { FlatList, View } from "react-native"
 import { Button, Text } from "react-native-paper"
 import Banner from "../Component/Banner"
 import Footer from "../Component/Footer"
+
 export default function PathTracking({navigation}){
     const handleTrackResult = ()=>{
         navigation.navigate('PathTracResult')
     }
+    const Data=[
+        {id:1,name:'A0000000000000001'},
+        {id:2,name:'A0000000000000001'},
+        {id:3,name:'A0000000000000001'},
+        {id:4,name:'A0000000000000001'},
+    ]
+    //取得渲染函數
+    const renderItem= ({item})=>(
+        <View
+        className='mt-4'
+        >
+            <Button 
+            mode='contained'
+            onPress={handleTrackResult}>
+            <Text className='text-white'>{item.name}</Text></Button>
+        </View>
+    )
     return(
         <View className='flex-1'>
-        <Banner/>
-        <View className='grow'>
-        <Text
-        variant="headlineMedium"
-        className="self-center mt-4"
-        >表單申報</Text>
-
-        <Button onPress={handleTrackResult}><Text>表單1</Text></Button>
-        <Button><Text>表單2</Text></Button>
-        <Button><Text>表單3</Text></Button>
-        <Button><Text>表單4</Text></Button>
-        </View>
-        <Footer/>
+            <Banner/>
+            <View className='grow'>
+                <Text
+                variant="headlineMedium"
+                className="self-center mt-4"
+                >
+                    表單申報
+                </Text>
+                <View
+                className='mx-10 mt-4 '
+                >
+                    <FlatList
+                    data={Data} // data
+                    renderItem={renderItem} // 選染列表的函數
+                    keyExtractor={item => item.id} // key
+                    />
+                </View>
+            </View>
+            
+            <Footer/>
         </View>
     )
 }
